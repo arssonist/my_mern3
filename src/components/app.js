ed15aada43e497c0cf3c3c5380355ea6c96804ac
Your Name <arssonist@yahoo.com>
2017-03-15 15:48:07 -0400

    add state with stage 2 styntax

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Header from './header'
 
-const App = (props) => {
-  return(
-    <div className="container">
-      <Header message="Naming Contests"/>
-      <div>{props.message}</div>
-</div>
+
+class App extends React.Component {
+//--regular way-----
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       test: 42
+//     };
+//---STAGE 2----------
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+  render(){
+    return(
+      <div className= "App">
+        <Header message={this.state.pageHeader}/>
+        <div></div>
+  </div>
   )
+  }
 }
+//-------STATELESS--------
+// const App = (props) => {
+//   return(
+//     <div className="container">
+//       <Header message="Naming Contests"/>
+//       <div>{props.message}</div>
+// </div>
+//   )
+// }
 
 App.propTypes = {
   message: React.PropTypes.string.isRequired
